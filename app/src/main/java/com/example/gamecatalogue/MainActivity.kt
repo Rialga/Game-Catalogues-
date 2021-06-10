b439038e17b76f5e9211400a650680a6383b565b
Rialga <febrialgani@gmail.com>
2021-06-10 14:38:57 +0700

    done ui

--- app/src/main/java/com/example/gamecatalogue/MainActivity.kt
+++ app/src/main/java/com/example/gamecatalogue/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.gamecatalogue
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_about, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        when (id) {
+            R.id.about -> {
+                val intentAbout = Intent(this@MainActivity, AboutActivity::class.java)
+                startActivity(intentAbout);
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
